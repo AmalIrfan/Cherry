ae95ab184329fb6d99f78329d5ff164abc68b8ee
Amal Irfan <69129408+AmalIrfan@users.noreply.github.com>
2024-03-24 16:13:58 +0530

    First blog The first ever blog! the base.html is getting repeated, I need to keep in mind to organise the templates

--- blogs/views.py
+++ blogs/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'blogs/index.html')
+
+def blog(request, id):
+    return render(request, f'contents/{id}.html')
