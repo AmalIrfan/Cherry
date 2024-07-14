fae6ab27340b599fe2821c767b8913d740563d82
Amal Irfan <69129408+AmalIrfan@users.noreply.github.com>
2024-07-14 12:28:10 +0530

    Made DEBUG a environment variable

--- cherry/settings.py
+++ cherry/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = dc.config('CHERRY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = dc.config('DEBUG') == 'true'
 
 ALLOWED_HOSTS = [dc.config('CHERRY_ALLOWED_HOSTS')]
 
