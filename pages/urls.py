f4fe785a94e7a91715e87dfb4be300d21134265e
Amal Irfan <69129408+AmalIrfan@users.noreply.github.com>
2024-07-14 12:22:03 +0530

    Added login and logout

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.contrib.auth import urls as auth_urls
 from .views import home, home_redirect, about
 
 urlpatterns = [
@@ -7,4 +8,5 @@ urlpatterns = [
     path('home/', home_redirect),
     path('about/', about),
     path('admin/', admin.site.urls),
+    path('account/', include(auth_urls)),
 ]
